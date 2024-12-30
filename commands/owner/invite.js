e443b56fc55f38ae479bc1aeefd03be4f159e791
MineAbhi Gamerz <164729403+MineAbhiGamerz@users.noreply.github.com>
2024-12-31 00:43:09 +0530

    invite.js

--- commands/owner/invite.js
+++ commands/owner/invite.js
@@ -11,7 +11,7 @@ module.exports = {
         if (message.author.id === ownerid) {
         let guild = null;
 
-        if (!args[0]) return message.channel.send("Enter Guild Name or Guild ID of where you want Invite Link.")
+        if (!args[0]) return message.channel.send("<a:Invite:1317867093537849364> [Invite Link]() And <:support:1318279495819595867> [Support](https://discord.gg/fZQUcabh6b).")
 
         if(args[0]){
             let fetched = bot.guilds.cache.find(g => g.name === args.join(" "));
