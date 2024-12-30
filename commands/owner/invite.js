66ba4d983728350171406fa38c1a2aa708eb6a82
MineAbhi Gamerz <164729403+MineAbhiGamerz@users.noreply.github.com>
2024-12-31 00:44:04 +0530

    invite.js

--- commands/owner/invite.js
+++ commands/owner/invite.js
@@ -11,7 +11,7 @@ module.exports = {
         if (message.author.id === ownerid) {
         let guild = null;
 
-        if (!args[0]) return message.channel.send("<a:Invite:1317867093537849364> [Invite Link]() And <:support:1318279495819595867> [Support](https://discord.gg/fZQUcabh6b).")
+        if (!args[0]) return message.channel.send("<a:Invite:1317867093537849364> [Invite Link](https://discord.com/oauth2/authorize?client_id=1302563639882158091&permissions=8&integration_type=0&scope=bot) And <:support:1318279495819595867> [Support](https://discord.gg/fZQUcabh6b).")
 
         if(args[0]){
             let fetched = bot.guilds.cache.find(g => g.name === args.join(" "));
